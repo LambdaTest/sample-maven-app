e9e8cdda2c628383908eabbddaa2329e8463cb76
Sushobhit <sushobhitd@lambdatest.com>
2019-10-18 19:35:28 +0530

    first working commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        Thread.sleep(10);
+        System.out.println( "Hello World!" );
+    }
+}
